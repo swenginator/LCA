7432362fc54c28a53cf6f031bd2b388660a2c776
swenginator <72451636+swenginator@users.noreply.github.com>
2020-10-14 22:47:22 +0100

    Add simple main class for Java

--- java/LCA.java
+++ java/LCA.java
@@ -3,13 +3,18 @@ import java.util.Set;
 
 public class LCA {
 
-    public static class Node<V> {
-        public final Set<Node<V>> children = new HashSet<>();
-        public final V data;
+    public static void main(String... args) {
+        // Create a simple test tree
+        Node<Integer> root = new Node<>(0);
+        Node<Integer> node1 = new Node<>(1);
+        Node<Integer> node2 = new Node<>(2);
 
-        public Node(V data) {
-            this.data = data;
-        }
+        root.children.add(node1);
+        root.children.add(node2);
+
+        Node<Integer> lca = getLCA(root, node1, node2);
+        System.out.println("LCA: " + lca.data);
+
     }
 
     public static <V> Node<V> getLCA(Node<V> root, Node<V> nodea, Node<V> nodeb) {
@@ -30,5 +35,19 @@ public class LCA {
         }
 
         return lastFoundNode;
+    }
+
+    /**
+     * A node of a n-ary tree. Has children and data. The key is equivalent to the reference.
+     *
+     * @param <V>
+     */
+    public static class Node<V> {
+        public final Set<Node<V>> children = new HashSet<>();
+        public final V data;
+
+        public Node(V data) {
+            this.data = data;
+        }
     }
 }
