b3a692c2f30830f77c3d1c5b5e65fb412edf0934
swenginator <72451636+swenginator@users.noreply.github.com>
2020-10-20 17:06:22 +0100

    Remove main method from LCA class

--- src/main/java/LCA.java
+++ src/main/java/LCA.java
@@ -4,24 +4,15 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class LCA {
-
-    public static void main(String... args) {
-        Node<Integer> root = new Node<>(0);
-        Node<Integer> node1 = new Node<>(1);
-        Node<Integer> node2 = new Node<>(2);
-        Node<Integer> node3 = new Node<>(3);
-        Node<Integer> node4 = new Node<>(4);
-
-        root.children.add(node1);
-        root.children.add(node2);
-        node1.children.add(node3);
-        node1.children.add(node4);
-
-        Node<Integer> lca = getLCA(root, node3, node4);
-        System.out.println("LCA: " + lca.data);
-
-    }
-
+    /**
+     * Get the Lowest Common Ancestor of any two nodes in an n-ary tree.
+     *
+     * @param root  The root node of the n-ary tree
+     * @param nodea The first node to find the ancestor of
+     * @param nodeb The second node to find the ancestor of
+     * @param <V>   The type of the nodes the tree holds
+     * @return The ancestor of the two input nodes, or null if not found
+     */
     public static <V> Node<V> getLCA(Node<V> root, Node<V> nodea, Node<V> nodeb) {
         if (root == null || nodea == root || nodeb == root) return root;
 
