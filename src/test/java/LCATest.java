7027bc920c7007370bb834d6ef68491ff04fca8d
swenginator <72451636+swenginator@users.noreply.github.com>
2020-10-20 18:16:31 +0100

    Test against Node key and not value

--- src/test/java/LCATest.java
+++ src/test/java/LCATest.java
@@ -1,6 +1,7 @@
 package test.java;
 
 import main.java.LCA;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static main.java.LCA.getLCA;
@@ -8,77 +9,51 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 class LCATest {
-
     /**
      * Test with a fairly simple tree structure
-     * Find ancestor of 3 and 4, should be 1
      * (0)
-     * (1)       (2)
+     * (1)   (2)
      * (3)  (4)
+     */
+    LCA.Node<Integer> root = new LCA.Node<>(0);
+    LCA.Node<Integer> node1 = new LCA.Node<>(1);
+    LCA.Node<Integer> node2 = new LCA.Node<>(2);
+    LCA.Node<Integer> node3 = new LCA.Node<>(3);
+    LCA.Node<Integer> node4 = new LCA.Node<>(4);
+
+    @BeforeEach
+    void setupTree() {
+        root.children.add(node1);
+        root.children.add(node2);
+        node1.children.add(node3);
+        node1.children.add(node4);
+    }
+
+    /**
+     * Find ancestor of 3 and 4, should be 1
      */
     @Test
     void getLCATest3HighTree1Ancestor() {
-        LCA.Node<Integer> root = new LCA.Node<>(0);
-        LCA.Node<Integer> node1 = new LCA.Node<>(1);
-        LCA.Node<Integer> node2 = new LCA.Node<>(2);
-        LCA.Node<Integer> node3 = new LCA.Node<>(3);
-        LCA.Node<Integer> node4 = new LCA.Node<>(4);
-
-        root.children.add(node1);
-        root.children.add(node2);
-        node1.children.add(node3);
-        node1.children.add(node4);
-
         LCA.Node<Integer> lca = getLCA(root, node3, node4);
-        assertEquals(1, lca.data);
+        assertEquals(node1, lca);
     }
 
     /**
-     * Test with a fairly simple tree structure
      * Find ancestor of 3 and 2, should be 0
-     * (0)
-     * (1)       (2)
-     * (3)  (4)
      */
     @Test
     void getLCATest3HighTree0Ancestor() {
-        LCA.Node<Integer> root = new LCA.Node<>(0);
-        LCA.Node<Integer> node1 = new LCA.Node<>(1);
-        LCA.Node<Integer> node2 = new LCA.Node<>(2);
-        LCA.Node<Integer> node3 = new LCA.Node<>(3);
-        LCA.Node<Integer> node4 = new LCA.Node<>(4);
-
-        root.children.add(node1);
-        root.children.add(node2);
-        node1.children.add(node3);
-        node1.children.add(node4);
-
         LCA.Node<Integer> lca = getLCA(root, node3, node2);
-        assertEquals(0, lca.data);
+        assertEquals(root, lca);
     }
 
     /**
-     * Test with a fairly simple tree structure
      * Find ancestor of root and root node
-     * (0)
-     * (1)       (2)
-     * (3)  (4)
      */
     @Test
     void getLCATest3HighTreeRootAncestor() {
-        LCA.Node<Integer> root = new LCA.Node<>(0);
-        LCA.Node<Integer> node1 = new LCA.Node<>(1);
-        LCA.Node<Integer> node2 = new LCA.Node<>(2);
-        LCA.Node<Integer> node3 = new LCA.Node<>(3);
-        LCA.Node<Integer> node4 = new LCA.Node<>(4);
-
-        root.children.add(node1);
-        root.children.add(node2);
-        node1.children.add(node3);
-        node1.children.add(node4);
-
         LCA.Node<Integer> lca = getLCA(root, root, root);
-        assertEquals(0, lca.data);
+        assertEquals(root, lca);
     }
 
     /**
@@ -100,7 +75,7 @@ class LCATest {
         root.children.add(node1);
 
         LCA.Node<Integer> lca = getLCA(root, root, node1);
-        assertEquals(0, lca.data);
+        assertEquals(root, lca);
     }
 
     /**
@@ -113,6 +88,6 @@ class LCATest {
         root.children.add(root);
 
         LCA.Node<Integer> lca = getLCA(root, root, root);
-        assertEquals(0, lca.data);
+        assertEquals(root, lca);
     }
 }
